87932c0ef666eb942b103280647bfda91587308d
Aushah Gowhar <waniaushahgowhar@gmail.com>
2026-03-08 21:19:44 +0530

    implemented cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,10 @@ const isAdmin = require("./middlewares/isAdmin.middleware.js");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
